e936a01d9c9abf62bc31a6e53477c92afd72cf06
창환 <hwan09290@naver.com>
2021-11-02 14:55:20 +0900

    .

--- Seodery/src/main/webapp/resources/script/login/login.js
+++ Seodery/src/main/webapp/resources/script/login/login.js
@@ -1,3 +1,36 @@
-/**
- * 
- */
+	/* 네비게이션 바 마우스 포커스 이벤트 */ 
+	$(function(){
+		gnbSlideInit();
+	});
+	
+	
+	function gnbSlideInit() {
+		//gnb (상위메뉴에 마우스 포커스)
+	    var current = false;
+		
+	    $('.gnb>ul>li').on('mouseenter focusin',function(){
+	       current = true;
+	       liObj = $(this)
+	       setTimeout(function() {
+	             if(current == true) {
+	                 $('.gnb>ul>li').removeClass('on'); 
+	                 //liObj.addClass('on');
+	             //alert();
+	             $('.gnb_sub, .gnb_subbox').stop().slideDown();
+	             }
+	         }, 50);
+	    });
+	    
+	    
+	    $('.gnb>ul>li').on('mouseleave focusout',function(){
+	       current = false;
+	       setTimeout(function() {
+	          if(current == false) {
+	             $('.gnb>ul>li').removeClass('on');
+	             $('.gnb_sub, .gnb_subbox').stop().slideUp();
+	          }
+	       }, 50); 
+	    });
+	    
+	} 
+	
