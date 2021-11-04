b8f6f904ae374da1ae89bb1769393573b71e013c
창환 <hwan09290@naver.com>
2021-11-05 03:25:06 +0900

    ..

---
+++ Seodery/src/main/java/com/gdj37/seodery/ur/controller/Service.java
@@ -0,0 +1,12 @@
+package com.gdj37.seodery.ur.controller;
+
+import java.util.HashMap;
+
+public class Service {
+
+	public static HashMap<String, Object> getMemberData(Object attribute) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
